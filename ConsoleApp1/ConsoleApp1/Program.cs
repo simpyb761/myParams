2b0e8626bc34c871393cf7f7015468392d887cda
simpyb761 <simpyb761@gmail.com>
2023-02-09 17:37:47 -0500

    Completed Program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace myParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            WriteLine("How many random numbers would you like to generate: ");
+            int arraySize = int.Parse(ReadLine());
+            WriteLine("Your Random integers are!");
+            int[] randomInts = new int[arraySize];
+            for (int x = 0; x < arraySize; x++)
+            {
+                randomInts[x] = randomNumber.Next(1, 10);
+            }
+            for (int x = 0; x < arraySize; x++)
+            {
+                Console.WriteLine(randomInts[x]);
+            }
+            WriteLine($"Your random integers added together equal {Add(randomInts)}");
+            WriteLine($"Your random integers multiplied together equal {Multiply(randomInts)}");
+        }
+        static int Add(params int[] ints)
+        {
+            int calculatedNumber = 0;
+            for (int x = 0; x < ints.Length; x++)
+            {
+                calculatedNumber += ints[x];
+            }
+            return calculatedNumber;
+        }
+        static int Multiply(params int[] ints)
+        {
+            int calculatedNumber = 1;
+            for (int x = 0; x < ints.Length; x++)
+            {
+                calculatedNumber *= ints[x];
+            }
+            return calculatedNumber;
+        }
+    }
+}
